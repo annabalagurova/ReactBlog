973f5f3f3bd8deadf423088ef5ec932b7fff1298
Anna <anna.balagurova17@gmail.com>
2026-05-25 21:12:21 +0700

    initial edit

--- src/pages/NewsFeed.jsx
+++ src/pages/NewsFeed.jsx
@@ -1,24 +1,42 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const ARTICLES_DATA = [
     {
         id: 'future-of-js',
         title: 'Статья 1',
-        description: 'Описание статьи 1'
+        description: 'Описание статьи 1',
+        authorId: 'system',
+        authorName: 'Редакция'
     },
     {
         id: 'css-modules',
         title: 'Статья 2',
-        description: 'Описание статьи 2'
+        description: 'Описание статьи 2',
+        authorId: 'system',
+        authorName: 'Редакция'
     },
     {
         id: 'react-router-v6',
         title: 'Статья 3',
-        description: 'Описание статьи 3'
+        description: 'Описание статьи 3',
+        authorId: 'system',
+        authorName: 'Редакция'
     }
 ];
 
 function NewsFeed () {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        const savedArticles = localStorage.getItem('blog_articles');
+        if (savedArticles){
+            setArticles(JSON.parse(savedArticles));
+        }else{
+            localStorage.setItem('blog_articles', JSON.stringify(ARTICLES_DATA));
+        }
+    }, []);
+
     return (
         <>
             <h1>Лента свежих новостей</h1>
